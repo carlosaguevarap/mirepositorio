79d5252b41dfbeff33dd53e6ace514c37a609c36
Wilmer Orlando Guamani Proano <WILMER.GUAMANI@pampaenergia.com>
2018-07-10 22:23:04 -0500

    Add file Pandas_Correlacion.py

---
+++ src/Pandas_Correlacion.py
@@ -0,0 +1,19 @@
+from pandas import pandas as pd
+import numpy as np
+
+def read_file(filepath,columns_names):
+    df=pd.DataFrame()
+    if(columns_names is None):
+        df = pd.read_csv(filepath,parse_dates=[['Date', 'Time']])
+    else:
+        df = pd.read_csv(filepath,parse_dates=[['Date', 'Time']],usecols=columns_names)
+    df.index = df["Date_Time"]
+
+    return df;
+
+def main():
+    print("Correlacion Humidity - TemperatureC - LDR")
+    df=read_file('../data/Data_Planta.csv',None);
+    df=df.corr()
+    print(df)
+main()
